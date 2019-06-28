601a0ddd3a29cc1454e31949c52f5be5f8d5d710
Norman Denayer <norman.denayer@netaxis.be>
2019-06-28 11:37:07 +0200

    backport babel-polyfill replacement

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import "babel-polyfill";
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+
 import "url-polyfill";
 import "isomorphic-fetch";
 import React, { Component } from 'react';
