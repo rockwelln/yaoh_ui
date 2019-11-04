f5deec8e60e51dc0f2f45ee81d9dd9af60e277ff
Norman Denayer <norman.denayer@netaxis.be>
2019-11-04 09:34:56 +0100

    keep up with a62a964

--- src/components/GroupPage/DeleteModal.js
+++ src/components/GroupPage/DeleteModal.js
@@ -29,13 +29,9 @@ class DeleteModal extends Component {
       )
       .then(res =>
         res
-          ? (this.setState({ deleting: false }),
-            onClose && onClose(true),
-            this.props.history.push(
-              `/provisioning/${this.props.match.params.gwName}/tenants/${
-                this.props.match.params.tenantId
-              }`
-            ))
+          ? this.props.history.push(
+              `/provisioning/${this.props.match.params.gwName}/tenants/${this.props.match.params.tenantId}`
+            )
           : this.setState({ deleting: false })
       );
   }
--- src/components/GroupPage/Tabs/Users/User.js
+++ src/components/GroupPage/Tabs/Users/User.js
@@ -16,7 +16,7 @@ class User extends Component {
     const { showDelete } = this.state;
     return (
       <tr key={user.userId}>
-        <td>
+          {/* <td>
           <Checkbox
             checked={user.userChecked}
             className={"table-checkbox"}
@@ -24,12 +24,10 @@ class User extends Component {
               this.props.handleSingleCheckboxClick(user.userId);
             }}
           />
-        </td>
+        </td> */}
         <td>
           <Link
-            to={`/provisioning/${
-              this.props.match.params.gwName
-            }/tenants/${tenantId}/groups/${groupId}/users/${user.userId}`}
+            to={`/provisioning/${this.props.match.params.gwName}/tenants/${tenantId}/groups/${groupId}/users/${user.userId}`}
           >
             {user.userId}
           </Link>
