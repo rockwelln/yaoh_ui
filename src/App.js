rockwelln/yaoh_ui

commit 10d05ec6366490ae2857e8025a26e0a978113d21

update dependencies and drop react-cookie

src/App.js
@@ -25,7 +25,6 @@ import {BrowserRouter as Router, Link, Redirect, Route, Switch} from 'react-rout
 import {LinkContainer} from 'react-router-bootstrap';
 import NotificationSystem from 'react-notification-system';
 
-import {withCookies} from 'react-cookie';
 import {FormattedMessage} from 'react-intl';
 
 import AsyncApioHelp from './async-apio-help';
@@ -44,11 +43,11 @@ import LocalQueues from "./system/queues";
 import {
     API_URL_PREFIX,
     AuthServiceManager,
-    checkStatus,
-    fetch_get,
+    checkStatus, createCookie,
+    fetch_get, getCookie,
     NotificationsManager,
     parseJSON,
-    ProvProxiesManager
+    ProvProxiesManager, removeCookie
 } from "./utils";
 import Databases from "./system/databases_mgm";
 import {AuditLogs} from "./system/audit";
@@ -529,12 +528,14 @@ class App extends Component {
 
     componentDidMount() {
         this.getDatabaseStatus();
-        this.props.cookies.get('auth_sso') === '1' && !sso_auth_service.isLoggedIn() && sso_auth_service.signinSilent();
+        // this.props.cookies.get('auth_sso') === '1' && !sso_auth_service.isLoggedIn() && sso_auth_service.signinSilent();
+        getCookie('auth_sso') === '1' && !sso_auth_service.isLoggedIn() && sso_auth_service.signinSilent();
     }
 
     updateToken(token, sso_auth) {
         AuthServiceManager.loadToken(token);
-        this.props.cookies.set('auth_sso', sso_auth?'1':'0', {path: '/'});  // maxAge = 24hours
+         // this.props.cookies.set('auth_sso', sso_auth?'1':'0', {path: '/'});  // maxAge = 24hours
+        createCookie("auth_sso", sso_auth?'1':'0', 1, '/');  // maxAge = 24hours
     }
 
     logout() {
@@ -543,8 +544,10 @@ class App extends Component {
         console.log('logout');
         AuthServiceManager.logout();
         // this.props.cookies.remove("auth_token", { path: '/' });
-        this.props.cookies.remove("auth_sso", { path: '/' });
-        this.props.cookies.remove("user_language", { path: '/' });
+        // this.props.cookies.remove("auth_sso", { path: '/' });
+        // this.props.cookies.remove("user_language", { path: '/' });
+        removeCookie("auth_sso");
+        removeCookie("user_language");
         sso_auth_service.removeUser().then(() => this.props.onLanguageUpdate(undefined));
     }
 
@@ -803,7 +806,7 @@ class App extends Component {
                                    <LocalUserProfile
                                        user_info={user_info}
                                        auth_token={auth_token}
-                                       onUserInfoChanged={()=> this.setState({user_info:undefined})}
+                                       onUserInfoChanged={()=> window.location.reload()}
                                        notifications={this._notificationSystem.current}
                                        {...props} />
                                )}
@@ -842,4 +845,4 @@ class App extends Component {
     }
 }
 
-export default withCookies(App);
+export default App;

src/index.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import {CookiesProvider, withCookies} from 'react-cookie';
 import {IntlProvider} from 'react-intl';
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
@@ -18,6 +17,7 @@ import mainReducer from "./store/reducers";
 
 //import en_GB from './translations/en.json';
 import fr_FR from './translations/fr.json';
+import {createCookie, getCookie, removeCookie} from "./utils";
 
 //addLocaleData(en);
 //addLocaleData(fr);
@@ -48,7 +48,7 @@ class AppWithIntl extends Component {
     _getLocale() {
         let locale = this.state?this.state.locale:undefined;
         if(locale === undefined) {
-            locale = this.props.cookies.get("user_language");
+            locale = getCookie("user_language");
         }
         if(locale === undefined || locale === "undefined") {
             locale = navigator.language.substr(0, 2);
@@ -59,10 +59,12 @@ class AppWithIntl extends Component {
     changeLanguage(locale) {
         if(locale !== this.state.locale) {
             this.setState({locale: locale});
-            if(locale === undefined)
-                this.props.cookies.remove('user_language', {path: '/'});
-            else
-                this.props.cookies.set('user_language', locale, {path: '/'});
+            if(locale === undefined) {
+                removeCookie('user_language')
+            } else {
+                createCookie('user_language', locale, null, '/');
+                window.location.reload();
+            }
         }
     }
 
@@ -77,7 +79,6 @@ class AppWithIntl extends Component {
         );
     }
 }
-let AppWithIntlAndCookies = withCookies(AppWithIntl);
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -89,9 +90,7 @@ const store = createStore(
 
 ReactDOM.render(
     <Provider store={store}>
-        <CookiesProvider>
-            <AppWithIntlAndCookies/>
-        </CookiesProvider>
+        <AppWithIntl/>
     </Provider>, document.getElementById('root')
 );
 unregister();

src/utils.js
@@ -3,7 +3,7 @@ export const API_URL_PREFIX = process.env.NODE_ENV === 'production'?window.locat
 export const STATIC_URL_PREFIX = process.env.NODE_ENV === 'production'?window.location.origin:'http://127.0.0.1:3000';
 export const API_URL_PROXY_PREFIX = '/api/v01/apio/sync';
 
-function getCookie(name) {
+export function getCookie(name) {
   var value = "; " + document.cookie;
   var parts = value.split("; " + name + "=");
   if (parts.length === 2)
@@ -13,7 +13,7 @@ function getCookie(name) {
       .shift();
 }
 
-function createCookie(name,value,days,path) {
+export function createCookie(name,value,days,path) {
 	var expires = "";
     if (days) {
 		var date = new Date();
@@ -26,7 +26,7 @@ function createCookie(name,value,days,path) {
 	document.cookie = name+"="+value+expires+"; path="+path;
 }
 
-function removeCookie(name) {
+export function removeCookie(name) {
     createCookie(name,"",-1);
 }
 

commit afa8dc8a016fe18500fe323c9e0485988d7de393

fix bug in login page (freezing) introduced with 10d05ec (drop react-cookie)

src/App.js
@@ -534,8 +534,9 @@ class App extends Component {
 
     updateToken(token, sso_auth) {
         AuthServiceManager.loadToken(token);
-         // this.props.cookies.set('auth_sso', sso_auth?'1':'0', {path: '/'});  // maxAge = 24hours
+        // this.props.cookies.set('auth_sso', sso_auth?'1':'0', {path: '/'});  // maxAge = 24hours
         createCookie("auth_sso", sso_auth?'1':'0', 1, '/');  // maxAge = 24hours
+        this.setState({user_info: this.state.user_info});
     }
 
     logout() {