851e93bbdfa379f9294440139c0c79e9fe55895e
Norman Denayer <norman.denayer@netaxis.be>
2020-04-09 14:25:42 +0200

    unregister for direct URL's containing /api

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -29,9 +29,13 @@ export default function register() {
       return;
     }
 
-    window.addEventListener('load', () => {
+    window.addEventListener('load', event => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
+      console.log("window.location.pathname", window.location.pathname);
+      if ( event.request.url.indexOf( '/api/' ) !== -1 ) {
+        return false;
+      }
       if (!isLocalhost) {
         // Is not local host. Just register service worker
         registerValidSW(swUrl);
