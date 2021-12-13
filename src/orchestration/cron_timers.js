db9f045c71e8e9e98a8e23b5cb732515fbafda4e
Norman Denayer <norman.denayer@netaxis.be>
2021-12-13 14:20:25 +0100

    revisit cron timer page

--- src/orchestration/cron_timers.js
+++ src/orchestration/cron_timers.js
@@ -1,13 +1,11 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Breadcrumb from "react-bootstrap/lib/Breadcrumb";
 import {FormattedMessage} from "react-intl";
 import Panel from "react-bootstrap/lib/Panel";
 import ButtonToolbar from "react-bootstrap/lib/ButtonToolbar";
-import {ApioDatatable} from "../utils/datatable";
 import Button from "react-bootstrap/lib/Button";
 import Modal from "react-bootstrap/lib/Modal";
 import update from "immutability-helper";
-import {Search} from "../utils/common";
 import {fetch_delete, fetch_get, fetch_post, fetch_put, NotificationsManager} from "../utils";
 import Checkbox from "react-bootstrap/lib/Checkbox";
 import Form from "react-bootstrap/lib/Form";
@@ -21,6 +19,7 @@ import Glyphicon from "react-bootstrap/lib/Glyphicon";
 import Select from "react-select";
 import InputGroup from "react-bootstrap/lib/InputGroup";
 import {DeleteConfirmButton} from "../utils/deleteConfirm";
+import {fetchActivities} from "./activity-editor";
 
 
 const validateCronEntry = (timer) => {
@@ -450,166 +449,156 @@ function deleteCronTimer(id, onSuccess) {
     ))
 }
 
+function fetchCronTimers(onSuccess) {
+  fetch_get(`/api/v01/timers/cron`)
+        .then(t => {
+            onSuccess && onSuccess(t.timers);
+        })
+        .catch(error => NotificationsManager.error(
+            <FormattedMessage id="timer-fetch-failed" defaultMessage="Fetch timers failed!" />,
+            error.message
+    ))
+}
 
-export default class CronTimers extends Search {
-    static defaultProps = update(Search.defaultProps, {'$merge': {
-        searchUrl: '/api/v01/timers/cron',
-        collectionName: 'timers',
-        defaultSortingSpec: [{field: 'job_id', direction: 'asc'}],
-    }});
+function enable(timer_id, enabled, onSuccess) {
+    fetch_put(`/api/v01/timers/cron/${timer_id}`, {enabled: enabled})
+        .then(() => {
+            NotificationsManager.success(
+                <FormattedMessage id="update-schedule-done" defaultMessage="Schedule saved!"/>
+            );
+            onSuccess && onSuccess();
+        })
+        .catch(error => NotificationsManager.error(
+            <FormattedMessage id="update-schedule-failed" defaultMessage="Failed to update schedule"/>,
+            error.message
+        ));
+}
 
-    constructor(props) {
-        super(props);
-        this.state.activities = [];
-    }
-    fetchActivities() {
-        fetch_get('/api/v01/activities')
-            .then(data => !this.cancelLoad && this.setState({activities: data.activities}))
-            .catch(error => NotificationsManager.error(
-                <FormattedMessage id="fetch-activities-failed" defaultMessage="Failed to fetch activities"/>,
-                error.message
-            ));
-    }
+function selectActivity(timer_id, activity_id, onSuccess) {
+    fetch_put(`/api/v01/timers/cron/${timer_id}`, {activity_id: activity_id?parseInt(activity_id, 10): null})
+        .then(() => {
+            NotificationsManager.success(
+                <FormattedMessage id="update-schedule-done" defaultMessage="Schedule saved!"/>
+            );
+            onSuccess && onSuccess();
+        })
+        .catch(error => NotificationsManager.error(
+            <FormattedMessage id="update-schedule-failed" defaultMessage="Failed to update schedule"/>,
+            error.message
+        ));
+}
 
-    onSelectActivity(timer_id, activity_id) {
-        fetch_put(`/api/v01/timers/cron/${timer_id}`, {activity_id: activity_id?parseInt(activity_id, 10): null})
-            .then(() => {
-                NotificationsManager.success(
-                    <FormattedMessage id="update-schedule-done" defaultMessage="Schedule saved!"/>
-                );
-                this._refresh();
-            })
-            .catch(error => NotificationsManager.error(
-                <FormattedMessage id="update-schedule-failed" defaultMessage="Failed to update schedule"/>,
-                error.message
-            ));
-    }
+export default function CronTimers() {
+  const [timers, setTimers] = useState([]);
+  const [activities, setActivities] = useState([]);
 
-    onEnable(timer_id, enabled) {
-        fetch_put(`/api/v01/timers/cron/${timer_id}`, {enabled: enabled})
-            .then(() => {
-                NotificationsManager.success(
-                    <FormattedMessage id="update-schedule-done" defaultMessage="Schedule saved!"/>
-                );
-                this._refresh();
-            })
-            .catch(error => NotificationsManager.error(
-                <FormattedMessage id="update-schedule-failed" defaultMessage="Failed to update schedule"/>,
-                error.message
-            ));
-    }
+  useEffect(() => {
+    fetchActivities(setActivities);
+    fetchCronTimers(setTimers);
+  }, []);
 
-    componentDidMount() {
-        document.title = "Scheduler";
-        super.componentDidMount();
-        this.fetchActivities();
-    }
+  const _refresh = () => fetchCronTimers(setTimers);
+  const activitiesOptions = activities.sort((a, b) => a.name.localeCompare(b.name)).map(a => ({value: a.id, label: a.name}));
 
-    render() {
-        const {resources, sorting_spec, pagination, activities} = this.state;
-        const activitiesOptions = activities.sort((a, b) => a.name.localeCompare(b.name)).map(a => ({value: a.id, label: a.name}));
+  return (
+      <div>
+          <Breadcrumb>
+              <Breadcrumb.Item active><FormattedMessage id="orchestration" defaultMessage="Orchestration"/></Breadcrumb.Item>
+              <Breadcrumb.Item active><FormattedMessage id="job-scheduler" defaultMessage="Job scheduler"/></Breadcrumb.Item>
+          </Breadcrumb>
 
-        return (
-            <div>
-                <Breadcrumb>
-                    <Breadcrumb.Item active><FormattedMessage id="orchestration" defaultMessage="Orchestration"/></Breadcrumb.Item>
-                    <Breadcrumb.Item active><FormattedMessage id="job-scheduler" defaultMessage="Job scheduler"/></Breadcrumb.Item>
-                </Breadcrumb>
+          <Panel>
+              <Panel.Body>
+                  <ButtonToolbar>
+                      <NewCronTimer onClose={() => _refresh()} activities={activities} />
+                  </ButtonToolbar>
+              </Panel.Body>
+          </Panel>
 
-                <Panel>
-                    <Panel.Body>
-                        <ButtonToolbar>
-                            <NewCronTimer onClose={() => this._refresh()} activities={activities} />
-                        </ButtonToolbar>
-                    </Panel.Body>
-                </Panel>
-
-                <Panel>
-                    <Panel.Heading>
-                        <Panel.Title><FormattedMessage id="job-scheduler" defaultMessage="Job scheduler" /></Panel.Title>
-                    </Panel.Heading>
-                    <Panel.Body>
-                        <ApioDatatable
-                            sorting_spec={sorting_spec}
-                            headers={[
-                                {
-                                    title: '',
-                                    field: 'enabled',
-                                    sortable: true,
-                                    style: { width: 50 },
-                                    render: n => (
-                                        <Checkbox
-                                            checked={n.enabled}
-                                            onChange={e => e.preventDefault()}
-                                            onClick={e => {
-                                                e.preventDefault();
-                                                this.onEnable(n.id, e.target.checked);
-                                            }} />
-                                    )
-                                },
-                                {title: "job id", field: 'job_id', sortable: true},
-                                {title: "minute", field: 'minute', style: { width: 70 }},
-                                {title: "hour", field: 'hour', style: { width: 70 }},
-                                {title: "day", field: 'day', style: { width: 70 }},
-                                {title: "month", field: 'month', style: { width: 70 }},
-                                {title: "year", field: 'year', style: { width: 70 }},
-                                {
-                                    title: <FormattedMessage id="activity" defaultMessage="Activity"/>,
-                                    field: 'activity_id',
-                                    style: { width: 300 },
-                                    render: n => (
-                                        <InputGroup>
-                                            <Select
-                                                className="basic-single"
-                                                classNamePrefix="select"
-                                                value={n.activity_id && activitiesOptions.find(a => a.value === n.activity_id)}
-                                                isClearable={false}
-                                                isSearchable={true}
-                                                name="activity"
-                                                onChange={(value, action) => {
-                                                    if(["select-option", "clear"].includes(action.action)) {
-                                                      this.onSelectActivity(n.id, value && value.value);
-                                                    }
-                                                }}
-                                                options={activitiesOptions} />
-                                            <InputGroup.Button>
-                                                <Button
-                                                    disabled={n.activity_id === null}
-                                                    bsStyle="primary"
-                                                    onClick={() => {
-                                                        let win = window.open(`/transactions/config/activities/editor/${n.activity_id}`, '_blank');
-                                                        win.focus();
-                                                    }}
-                                                    style={{marginLeft: '5px'}}
-                                                >
-                                                    <Glyphicon glyph="eye-open"/>
-                                                </Button>
-                                            </InputGroup.Button>
-                                        </InputGroup>
-                                    )
-                                },
-                                {
-                                    title: '',
-                                    style: { width: "200px" },
-                                    render: n => (
-                                        <ButtonToolbar>
-                                            <UpdateTimer onClose={() => this._refresh()} timer={n}  activities={activities} />
-                                            <DeleteConfirmButton
-                                                resourceName={n.job_id}
-                                                style={{marginLeft: '5px', marginRight: '5px'}}
-                                                onConfirm={() => deleteCronTimer(n.id, () => this._refresh())} />
-                                        </ButtonToolbar>
-                                    )
-                                },
-                            ]}
-                            pagination={pagination}
-                            data={resources}
-                            onSort={s => this._refresh(undefined, s)}
-                            onPagination={p => this._refresh(p)}
-                        />
-                    </Panel.Body>
-                </Panel>
-            </div>
-        )
-    }
+          <Panel>
+              <Panel.Heading>
+                  <Panel.Title><FormattedMessage id="job-scheduler" defaultMessage="Job scheduler" /></Panel.Title>
+              </Panel.Heading>
+              <Panel.Body>
+                  <Table>
+                    <thead>
+                      <tr>
+                        <th/>
+                        <th>job id</th>
+                        <th>minute</th>
+                        <th>hour</th>
+                        <th>day</th>
+                        <th>month</th>
+                        <th>year</th>
+                        <th><FormattedMessage id="activity" defaultMessage="Activity"/></th>
+                        <th/>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {
+                      timers.map((n, i) => (
+                        <tr>
+                          <td>
+                            <Checkbox
+                              checked={n.enabled}
+                              onChange={e => e.preventDefault()}
+                              onClick={e => {
+                                  e.preventDefault();
+                                  enable(n.id, e.target.checked, _refresh);
+                              }} />
+                          </td>
+                          <td>{n.job_id}</td>
+                          <td style={{width: 70}}>{n.minute}</td>
+                          <td style={{width: 70}}>{n.hour}</td>
+                          <td style={{width: 70}}>{n.day}</td>
+                          <td style={{width: 70}}>{n.month}</td>
+                          <td style={{width: 70}}>{n.year}</td>
+                          <td style={{width: 300}}>
+                            <InputGroup>
+                                <Select
+                                    className="basic-single"
+                                    classNamePrefix="select"
+                                    value={n.activity_id && activitiesOptions.find(a => a.value === n.activity_id)}
+                                    isClearable={false}
+                                    isSearchable={true}
+                                    name="activity"
+                                    onChange={(value, action) => {
+                                        if(["select-option", "clear"].includes(action.action)) {
+                                          selectActivity(n.id, value && value.value, _refresh);
+                                        }
+                                    }}
+                                    options={activitiesOptions} />
+                                <InputGroup.Button>
+                                    <Button
+                                        disabled={n.activity_id === null}
+                                        bsStyle="primary"
+                                        onClick={() => {
+                                            let win = window.open(`/transactions/config/activities/editor/${n.activity_id}`, '_blank');
+                                            win.focus();
+                                        }}
+                                        style={{marginLeft: '5px'}}
+                                    >
+                                        <Glyphicon glyph="eye-open"/>
+                                    </Button>
+                                </InputGroup.Button>
+                            </InputGroup>
+                          </td>
+                          <td style={{width: "200px"}}>
+                            <ButtonToolbar>
+                              <UpdateTimer onClose={() => _refresh()} timer={n}  activities={activities} />
+                              <DeleteConfirmButton
+                                  resourceName={n.job_id}
+                                  style={{marginLeft: '5px', marginRight: '5px'}}
+                                  onConfirm={() => deleteCronTimer(n.id, () => _refresh())} />
+                            </ButtonToolbar>
+                          </td>
+                        </tr>
+                      ))
+                    }
+                    </tbody>
+                  </Table>
+              </Panel.Body>
+          </Panel>
+      </div>
+  )
 }
