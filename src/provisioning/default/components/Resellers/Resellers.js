8409b0192c7714f4c94fecff28f95549da7f4cbe
viktorsmakhtin <viktor.smakhtin@sharksw.com>
2022-11-30 11:45:07 +0200

    refactoring

--- src/provisioning/default/components/Resellers/Resellers.js
+++ src/provisioning/default/components/Resellers/Resellers.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router";
-import { Link } from "react-router-dom";
 
 import Table from "react-bootstrap/lib/Table";
 import Glyphicon from "react-bootstrap/lib/Glyphicon";
