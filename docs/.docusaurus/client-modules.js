bf85811955a0a23aaf419e2f0358490dfe3dc235
Norman Denayer <norman.denayer@netaxis.be>
2023-09-06 09:42:34 +0200

    remove docs

--- docs/.docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require('/Users/normandenayer/projects/sandbox/apio_core_auth/www/docs/node_modules/infima/dist/css/default/default.css'),
-  require('/Users/normandenayer/projects/sandbox/apio_core_auth/www/docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
-  require('/Users/normandenayer/projects/sandbox/apio_core_auth/www/docs/node_modules/@docusaurus/theme-classic/lib/nprogress'),
-  require('/Users/normandenayer/projects/sandbox/apio_core_auth/www/docs/src/css/custom.css'),
-  require('/Users/normandenayer/projects/sandbox/apio_core_auth/www/docs/node_modules/docusaurus-theme-redoc/dist/custom.css'),
-  require('/Users/normandenayer/projects/sandbox/apio_core_auth/www/docs/node_modules/docusaurus-theme-redoc/dist/global.js'),
-];
--- docs/docs/configuration/env.md
+++
@@ -1,9 +0,0 @@
----
-sidebar_position: 13
----
-
-# Environment variables
-
-APIO core can be configured with environment variables.
-
-Those variables are exposed to workflows in the `env` object. (e.g. `{{ env.MY_VARIABLE }}`)
