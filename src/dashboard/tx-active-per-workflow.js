3f29820cae943aff382bec33a906ae7813ef25ef
Norman Denayer <norman.denayer@netaxis.be>
2023-11-10 09:59:26 +0100

    change the active workflow dashboard panel to have a list with links to NP requests page

--- src/dashboard/tx-active-per-workflow.js
+++ src/dashboard/tx-active-per-workflow.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useCallback, useEffect, useState} from 'react';
 import Modal from 'react-bootstrap/lib/Modal';
 import {Doughnut} from 'react-chartjs-2';
 import {DashboardPanel} from './dashboard-panel';
@@ -6,20 +6,21 @@ import ColorHash from 'color-hash';
 
 import {fetch_get} from "../utils";
 import {FormattedMessage} from 'react-intl';
+import {DashboardCard} from "./dashboard-tiles";
+import queryString from "query-string";
+import {Link} from "react-router-dom";
 
 const REFRESH_CYCLE = 60;
 
 
-export default class ActiveTransactionsPerWorkflow extends Component {
+export class ActiveTransactionsPerWorkflow_BKP extends Component {
     constructor(props, context) {
         super(props, context);
         this.cancelLoad = false;
         this.state = {
             data: [],
-            names: {},
         };
         this.refresh = this.refresh.bind(this);
-        this.loadActivityNames = this.loadActivityNames.bind(this);
     }
 
     componentWillUnmount() {
@@ -30,7 +31,6 @@ export default class ActiveTransactionsPerWorkflow extends Component {
     componentDidMount() {
         this._refreshHandler = setInterval(this.refresh, REFRESH_CYCLE * 1000);
         this.refresh();
-        this.loadActivityNames();
     }
 
     refresh() {
@@ -44,30 +44,20 @@ export default class ActiveTransactionsPerWorkflow extends Component {
     shouldComponentUpdate(nextProps, nextState, nextContext) {
         const s1 = JSON.stringify(this.state.data);
         const s2 = JSON.stringify(nextState.data);
-        return s1 !== s2 || this.state.names !== nextState.names;
-    }
-
-  loadActivityNames() {
-        if(this.cancelLoad) return;
-
-        fetch_get('/api/v01/activities')
-        .then(data => !this.cancelLoad && this.setState({
-            names: data.activities.reduce((rv, a) => {rv[a.id] = a.name; return rv;}, {})
-        }))
-        .catch(console.error)
+        return s1 !== s2
     }
 
     render() {
-        const {data, names} = this.state;
+        const {data} = this.state;
         const onShowClose = () => {
             this.setState({showBig: false});
         };
         let colorHash = new ColorHash();
         const chartData = {
-            labels: data.sort((a, b) => b.counter - a.counter).map(d => `${names[d.activity_id]}: ${d.counter}`),
+            labels: data.sort((a, b) => b.counter - a.counter).map(d => `${d.workflow_name}: ${d.counter}`),
             datasets: [{
                 data: data.map(d => d.counter),
-                backgroundColor: data.map(d => colorHash.hex(names[d.activity_id] || "")),
+                backgroundColor: data.map(d => colorHash.hex(d.workflow_name || "")),
             }]
         };
         const chartOptions = {
@@ -102,4 +92,55 @@ export default class ActiveTransactionsPerWorkflow extends Component {
             </DashboardPanel>
         );
     }
+}
+
+
+export default function ActiveTransactionsPerWorkflow() {
+    const [data, setData] = useState([]);
+    // for future use may be...
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const refresh = useCallback(() => {
+        setLoading(true);
+        fetch_get('/api/v01/system/stats_active_per_wf')
+            .then(data => {
+                setData(data.requests);
+                setLoading(false);
+            })
+            .catch(error => {
+                setError(error);
+                setLoading(false);
+            });
+    }, []);
+
+    useEffect(() => {
+        refresh();
+        const interval = setInterval(refresh, REFRESH_CYCLE * 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <DashboardPanel
+            title={<FormattedMessage id='act-tx-workflow' defaultMessage='Active transactions / workflow'/>} >
+            {
+                data.sort((a, b) => b.counter - a.counter).map(d => (
+                  <Link to={{
+                    pathname: "/transactions/list", search: queryString.stringify({
+                      filter: JSON.stringify({
+                        activity_id: {model: 'instances', value: d.activity_id, op: 'eq'},
+                        status: {model: 'instances', value: 'ACTIVE', op: 'eq'},
+                      })
+                    })
+                  }}>
+                    <DashboardCard
+                        key={d.workflow_name}
+                        className={"bg-green-fade"}
+                        heading={d.workflow_name}
+                        number={d.counter} />
+                  </Link>
+                ))
+            }
+        </DashboardPanel>
+    );
 }
